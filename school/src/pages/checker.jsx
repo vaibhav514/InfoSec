ccd928b78f9ae0d73fd5be1bebf42c130d81a8e8
Hypix3l <laddhamanan0@gmail.com>
2024-05-03 02:32:50 +0530

    Improved Password Strenght checker and Base64 Encode

--- school/src/pages/checker.jsx
+++ school/src/pages/checker.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import PasswordStrengthBar from 'react-password-strength-bar';
 import BreadCrumps from "../components/BreadCrumps";
-
-
+import './checker.css';
 
 function calculateEntropy(password) {
   const symbols = "!@#$%^&*()_-+=~`[]{}|;:,.<>?";
@@ -40,50 +39,74 @@ function Checker() {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [entropy, setEntropy] = useState(0);
-  // const [strength, setStrength] = useState('Weak');
-
+  const [hasCapital, setHasCapital] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasSpecial, setHasSpecial] = useState(false);
 
   function handleChange(event) {
     const newPassword = event.target.value;
     setPassword(newPassword);
-    const newEntropy = calculateEntropy(newPassword);
-    // if (newEntropy >= 110) {
-    //   setStrength('Strong');
-    // } else if (newEntropy >= 70) {
-    //   setStrength('Good');
-    // } else if (newEntropy >= 50) {
-    //   setStrength('Fair');
-    // } else {
-    //   setStrength('Weak');
-    // }
+    const newEntropy = calculateEntropy(newPassword).toFixed(2);
     setEntropy(newEntropy);
+    setHasCapital(/[A-Z]/.test(newPassword));
+    setHasNumber(/[0-9]/.test(newPassword));
+    setHasSpecial(/[!@#$%^&*()_\-+=~`[\]{}|;:,.<>?]/.test(newPassword));
   }
 
   function toggleShowPassword() {
     setShowPassword(!showPassword);
   }
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(password);
+    handleShowBanner();
+  };
+
+  function SuccessBanner() {
+    return (
+      <div className="success-banner">
+        <p style={{color: 'black'}}>Copied Successfully!</p>
+      </div>
+    );
+  }
+  
+  const [showBanner, setShowBanner] = useState(false);
+
+  const handleShowBanner = () => {
+    setShowBanner(true);
+    setTimeout(() => {
+      setShowBanner(false);
+    }, 1500); // Hide the banner after 1.5 seconds
+  }
+
   return (
     <div>
-      <BreadCrumps page="Tools/Password Strength Checker" title="Password Strength Checker" />
-      <label>
-        Password:
-        <input
-          type={showPassword ? 'text' : 'password'}
-          value={password}
-          onChange={handleChange}
-        />
-        <PasswordStrengthBar password={password} />
-      <p className={`password-strength ${entropy >= 70 ? 'strong' : 'weak'}`}>
-        {/* {entropy >= 110 ? 'Strong' : entropy >= 70 ? 'Good' : entropy >= 50 ? 'Fair' : 'Weak'} */}
-      </p>
-      <br></br>
-      </label>
-      <button onClick={toggleShowPassword}>
-        {showPassword ? 'Hide' : 'Show'} Password
-      </button>
-      <p>Password Entropy: {entropy}</p>
-      {/* <p>Password Strength: {strength}</p> */}
+      <BreadCrumps page="Tools/Password Strength Checker" title="Password Strength Checker"/>
+      <div className="password-container">
+        <label>
+          Password:
+          <p className={`password-entropy ${entropy >= 95 ? 'strong' : entropy >= 70 ? 'good' : entropy >= 45 ? 'fair' : 'weak'}`} >Entropy: {entropy}</p>
+          <input
+            className="password-input"
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={handleChange}
+            maxLength={50}
+          />
+          <PasswordStrengthBar password={password} />
+          <div className="password-indicators">
+            <span className={`password-indicator ${hasCapital ? 'success' : 'failure'}`}>Capital</span>
+            <span className={`password-indicator ${hasNumber ? 'success' : 'failure'}`}>Number</span>
+            <span className={`password-indicator ${hasSpecial ? 'success' : 'failure'}`}>Special</span>
+          </div>
+          <button className="toggle-button" onClick={copyToClipboard}>Copy Password</button>
+          <button className="toggle-button" onClick={toggleShowPassword}>
+            {showPassword ? 'Hide' : 'Show'} Password
+          </button>
+          {showBanner && <SuccessBanner />}
+          <br />
+        </label>
+      </div>
     </div>
   );
 }
--- school/src/pages/tools.jsx
+++ school/src/pages/tools.jsx
@@ -1,24 +1,35 @@
 // In your Tools tab file (e.g., ToolsTab.js)
 import React from 'react';
 import BreadCrumps from "../components/BreadCrumps";
+import './tools.css';
+
+const buttons = [
+  { label: "Password Generator", link: "pg" },
+  { label: "Password Strength Checker", link: "checker" },
+  { label: "Base 64 Encoder", link: "en" },
+  { label: "Base 64 Decoder", link: "de" },
+  // Add more buttons as needed
+];
+
+const Button = ({ label, link }) => (
+  <div>
+    <h3>
+      <a href={link} className="button">{label}</a>
+    </h3>
+  </div>
+);
 
 function Tools() {
   return (
     <div>
       <BreadCrumps page="Tools" title="Tools" />
       <center>
-      <div>
-      <h3><a href="checker">Password Strength Checker</a></h3>
-      </div>
-      <div>
-        <h3><a href="pg">Password Generator</a></h3>
-      </div>
-      <div>
-        <h3><a href="en">Base 64 Encoder</a></h3>
-      </div>
-      <div>
-        <h3><a href="de">Base 64 Decoder</a></h3>
-      </div>
+      <div className="button-grid">
+          {/* Render buttons dynamically */}
+          {buttons.map((button, index) => (
+            <Button key={index} label={button.label} link={button.link} />
+          ))}
+        </div>
       </center>
     </div>
   );
